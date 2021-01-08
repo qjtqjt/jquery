47ac99ce146ddc718399608c7d071139721286e1
qinjianting <qjtqjt99@163.com>
2021-01-08 18:03:32 +0800

    歌曲数据提交

--- QQ音乐播放器/js/index.js
+++ QQ音乐播放器/js/index.js
@@ -1,4 +1,12 @@
-$(function() {
+$(document).ready(function(){
+    $(".content_in_list").niceScroll({
+        cursorcolor: "#333", 
+        cursoropacitymax: 0.5,
+    });
+    $(".right_botton_list").niceScroll({
+        cursorcolor: "#333", 
+        cursoropacitymax: 0.5,
+    });
     $(".list_title").hover(function() {
         // $(".content_in_list").mCustomScrollbar()
         //1.歌曲移入子菜单icon显示
@@ -12,4 +20,18 @@ $(function() {
         $(this).find(".del").stop().fadeOut(100);
         $(this).find(".time").stop().fadeIn(100);
     })
+    // 获取数据
+    getPlayerList()
+    function getPlayerList() {
+        $.ajax({
+            url: '../source/musiclist.json',
+            dataType: 'json',
+            success: function (data) {
+                console.log(data)
+            },
+            error: function (e) {
+                console.log(e)
+            }
+        })
+    }
 })
